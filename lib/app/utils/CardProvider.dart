5584af6da2a89a73fb57e506b7502e1bdc8711cc
Ambar Pandey <ambar.ara123@gmail.com>
2021-12-01 19:54:46 +0530

    add: like, dislike, superLike functionality

--- lib/app/utils/CardProvider.dart
+++ lib/app/utils/CardProvider.dart
@@ -8,22 +8,26 @@ class CardProvider extends ChangeNotifier {
   double _angle = 0;
 
   Offset get position => _position;
+
   bool get isDragging => _isDragging;
+
   double get angle => _angle;
+
   List<String> get imageUrls => _imageUrls;
+
   // Size get screenSize => _screenSize;
 
-  void start(DragStartDetails details){
+  void start(DragStartDetails details) {
     _isDragging = true;
 
     notifyListeners();
   }
 
-  CardProvider(){
+  CardProvider() {
     resetUsers();
   }
 
-  void update(DragUpdateDetails details){
+  void update(DragUpdateDetails details) {
     _position += details.delta;
     final x = _position.dx;
     _angle = 45 * x / _screenSize.width;
@@ -31,11 +35,28 @@ class CardProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void end(){
-    resetPosition();
+  void end() {
+    _isDragging = false;
+    notifyListeners();
+
+    final swipeType = getType();
+
+    switch (swipeType) {
+      case SwipeType.like:
+        like();
+        break;
+      case SwipeType.dislike:
+        disLike();
+        break;
+      case SwipeType.superLike:
+        superLike();
+        break;
+      default:
+        resetPosition();
+    }
   }
 
-  void resetPosition(){
+  void resetPosition() {
     _isDragging = false;
     _position = Offset.zero;
     _angle = 0;
@@ -58,4 +79,48 @@ class CardProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  SwipeType getType() {
+    if (_position.dx > 100) {
+      return SwipeType.like;
+    } else if (position.dx < -100) {
+      return SwipeType.dislike;
+    } else if (position.dy < -50 && position.dx.abs() < 20) {
+      return SwipeType.superLike;
+    }
+    return SwipeType.nothing;
+  }
+
+  void like() {
+    _angle = 20;
+    _position += Offset(_screenSize.width, 0);
+    _showNextCard();
+
+    notifyListeners();
+  }
+
+  void disLike() {
+    _angle = -20;
+    _position -= Offset(_screenSize.width, 0);
+    _showNextCard();
+
+    notifyListeners();
+  }
+
+  void superLike() {
+    _angle = 0;
+    _position -= Offset(0, _screenSize.height);
+    _showNextCard();
+
+    notifyListeners();
+  }
+
+  Future _showNextCard() async {
+    if (_imageUrls.isEmpty) return;
+
+    await Future.delayed(Duration(milliseconds: 300));
+    _imageUrls.removeLast();
+    resetPosition();
+  }
 }
+
+enum SwipeType { like, dislike, superLike, nothing }
